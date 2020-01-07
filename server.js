a51712bd1fcf2747ae2c1f05c40a45fe0f08f021
Johan Castillo <jcjohan2707@gmail.com>
2020-01-07 15:44:16 -0400

    Recibiendo la url que el cliente pide

--- server.js
+++ server.js
@@ -1,21 +1,46 @@
-//Imports or Requires.
+/*
+==========================
+== Imports or Requires. ==
+==========================
+*/
 const express = require('express');
 const colors = require('colors');
 
-//Server.
+/*
+=============
+== Server. ==
+=============
+*/
 const server = express();
+
+/*
+=================
+== Middlewares ==
+=================
+*/
+server.use((req,res, next) => console.log('Request url:', req.url))
+
+/* 
+==========
+== URLs ==
+========== 
+*/
 
 //Response for url: '/'
 server.get('/', (req,res) => res.send('<h1>Esta es la raíz del Servidor</h1>'))
 
-//Response fron url '/about'.
+//Response for url '/about'.
 server.get('/about', (req,res) => res.send('<h1>Aquí va el about</h1>'))
 
-//Response fron url '/login'.
+//Response for url '/login'.
 server.get('/login', (req,res) => res.send('<h1>Aquí va el login</h1>'))
 
-//Response 404
-server.get('*', (req, res) => res.send('<h1>404 Not found</h1>'))
+//Err 404 'Not Found'.
+server.get('*', (req, res) => res.send('<h1>404 "Not found"</h1>'))
 
-//Port and CallBack.
+/*
+========================
+== Port and CallBack. ==
+========================
+*/
 server.listen(3000, () => console.log("Server on 'localhost:3000'.".blue))
