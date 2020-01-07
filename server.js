81227442b5a66e9bd7800c65b06355b3568a2de6
Johan Castillo <jcjohan2707@gmail.com>
2020-01-07 14:12:26 -0400

    Se agrego el error 404

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ const colors = require('colors');
 const server = express();
 
 //Response for url: '/'
-server.get('/', (req,res) => res.send('<h1>Esta es la raíz del Servidor</h1>'));
+server.get('/', (req,res) => res.send('<h1>Esta es la raíz del Servidor</h1>'))
 
 //Response fron url '/about'.
-server.get('/about', (req,res) => res.send('<h1>Aquí va el about</h1>'));
+server.get('/about', (req,res) => res.send('<h1>Aquí va el about</h1>'))
 
 //Response fron url '/login'.
-server.get('/login', (req,res) => res.send('<h1>Aquí va el login</h1>'));
+server.get('/login', (req,res) => res.send('<h1>Aquí va el login</h1>'))
+
+//Response 404
+server.get('*', (req, res) => res.send('<h1>404 Not found</h1>'))
 
 //Port and CallBack.
-server.listen(3000, () => console.log("Server on 'localhost:3000'.".blue));
+server.listen(3000, () => console.log("Server on 'localhost:3000'.".blue))
